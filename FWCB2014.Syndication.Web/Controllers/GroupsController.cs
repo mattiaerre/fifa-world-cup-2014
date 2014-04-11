27842e8b0a27b456b932856c1fb03b1c81daad5e
mattia.richetto <mattia.richetto@gmail.com>
2014-04-11 09:41:57 +0100

    castle windows w/ web api (wip)

--- FWCB2014.Syndication.Web/Controllers/GroupsController.cs
+++ FWCB2014.Syndication.Web/Controllers/GroupsController.cs
@@ -1,7 +1,6 @@
 ﻿using FWCB2014.Domain.Core.Models;
 using FWCB2014.Domain.Core.Models.Query.Groups;
-using FWCB2014.Domain.Infrastructure.Repositories;
-using FWCB2014.Syndication.Infrastructure.Services;
+using FWCB2014.Domain.Core.Services;
 using System.Collections.Generic;
 using System.Web.Http;
 
@@ -9,10 +8,17 @@ namespace FWCB2014.Syndication.Web.Controllers
 {
   public class GroupsController : ApiController
   {
+    private readonly IGroupsService<GroupModel<TeamModel>> _service;
+
+
+    public GroupsController(IGroupsService<GroupModel<TeamModel>> service)
+    {
+      _service = service;
+    }
+
     public IEnumerable<GroupModel<TeamModel>> Get()
     {
-      var service = new JsonGroupsService(@"C:\Users\mattia.richetto\Dropbox\dotNet\prj\FWCB2014\FWCB2014.Application.Web\App_Data\Groups.json", new JsonCountryRepository(@"C:\Users\mattia.richetto\Dropbox\dotNet\prj\FWCB2014\FWCB2014.Application.Web\App_Data\Countries.json"));
-      var groups = service.GetAll();
+      var groups = _service.GetAll();
       return groups;
     }
   }
--- FWCB2014.Syndication.Web/Global.asax.cs
+++ FWCB2014.Syndication.Web/Global.asax.cs
@@ -1,7 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.Web;
+using System.Web.Http.Controllers;
+using System.Web.Http.Dispatcher;
+using Castle.MicroKernel.Registration;
+using Castle.MicroKernel.Resolvers.SpecializedResolvers;
+using Castle.Windsor;
+using Castle.Windsor.Installer;
+using FWCB2014.Domain.Core.Models;
+using FWCB2014.Domain.Core.Models.Query.Groups;
+using FWCB2014.Domain.Core.Repositories;
+using FWCB2014.Domain.Core.Services;
+using FWCB2014.Domain.Infrastructure.Repositories;
+using FWCB2014.Syndication.Infrastructure.Services;
+using FWCB2014.Syndication.Web.Plumbing;
 using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
@@ -9,8 +19,36 @@ using System.Web.Routing;
 
 namespace FWCB2014.Syndication.Web
 {
-  public class WebApiApplication : System.Web.HttpApplication
+  public class WebApiApplication : HttpApplication
   {
+    private static IWindsorContainer _container;
+
+    private static void BootstrapContainer(HttpConfiguration configuration)
+    {
+      _container = new WindsorContainer().Install(FromAssembly.This());
+
+      _container.Register(
+      Types.FromThisAssembly()
+      .BasedOn(typeof(ApiController))
+      .WithServices(typeof(ApiController))
+      .LifestyleTransient());
+
+      //var controllerFactory = new WindsorControllerFactory(_container.Kernel);
+      //ControllerBuilder.Current.SetControllerFactory(controllerFactory);
+
+      //_container.Kernel.Resolver.AddSubResolver(new CollectionResolver(_container.Kernel, true));
+      //var dependencyResolver = new WindsorDependencyResolver(_container);
+      //configuration.DependencyResolver = dependencyResolver;
+
+      configuration.Services.Replace(typeof(IHttpControllerActivator), new WindsorCompositionRoot(_container));
+
+      //_container.Register(
+      //  Component.For<IRepository<CountryModel>>().ImplementedBy<JsonCountryRepository>().DependsOn(new { jsonPath = Server.MapPath(@"~/App_Data/Countries.json") }));
+
+      //_container.Register(
+      //  Component.For<IGroupsService<GroupModel<TeamModel>>>().ImplementedBy<JsonGroupsService>().DependsOn(new { jsonPath = Server.MapPath(@"~/App_Data/Groups.json"), }));
+    }
+
     protected void Application_Start()
     {
       AreaRegistration.RegisterAllAreas();
@@ -18,6 +56,13 @@ namespace FWCB2014.Syndication.Web
       FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
       RouteConfig.RegisterRoutes(RouteTable.Routes);
       BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+      BootstrapContainer(GlobalConfiguration.Configuration);
+    }
+
+    protected void Application_End()
+    {
+      _container.Dispose();
     }
   }
 }
